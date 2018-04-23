fe11709590e9bedf685fde8479f1d24d432279ce
David <sheehy_d@ymail.com>
2018-04-23 23:25:27 +0100

    host commit

--- js/login.js
+++ js/login.js
@@ -16,91 +16,13 @@ firebase.auth().onAuthStateChanged(function(user){
     }
 ); 
 
-// firebase.auth().onAuthStateChanged(function(user){
-//         if(user){
-//             console.log('User Logged in :' + user );
-//             var data = localStorage.setItem('currentUser', user);
-//             //window.location.href ='main.html';
-//         }
-//         else{
-//             console.log('User Not Logged in');
-//         }
-        
-//     }
-// );
 
-// var email;
-
-// var password;
-
-// /*Log in to Account*/
-//     function signIn(){
-        
-        
-//         email = document.getElementById("loginEmail").value;
-//         password = document.getElementById("loginPassword").value;
-        
-//         if (email != "" && password != "") {
-
-//             firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-//                 // Handle Errors here.
-// 				alert(error.message);
-//         });
-        
-//         // if(email !="" && password !=""){
-//         //     firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-//         //       // Handle Errors here.
-              
-//         //       alert(error.message);
-              
-//         //     //var errorCode = error.code;
-//         //     //var errorMessage = error.message;
-//         //      /*if (errorCode === 'auth/wrong-password') {
-//         //         //$("#loginError").show().text(error.message);
-                
-//         //      }*/
-//         //     });
-//         // }
-//         }
-//     };
-    
-//     $("#login").click(
-//     function(){
-        
-//         //var username = $("#loginUsername").val();
-        
-//         var email = document.getElementById("loginEmail").value;//$("#loginEmail").val();
-        
-//         var password = document.getElementById("loginPassword").value; //$("loginPassword").val();
-        
-//         if(email !="" && password !=""){
-//             firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-//               // Handle Errors here.
-              
-//             var errorCode = error.code;
-//             var errorMessage = error.message;
-//              if (errorCode === 'auth/wrong-password') {
-              
-//                 $("#loginError").show().text(error.message);
-//              }
-//              else{
-//                  window.location.href = "/home.html";
-//                 $("#login").hide();
-//                 $("#logout").show();
-//              }
-                
-//             });
-//         }
-//     }
-// );
   
   var email;
   var password;
   
    $("#login").click(
     function(){
-        
-        //var username = $("#loginUsername").val();
         
         email = document.getElementById("loginEmail").value;
         
@@ -110,23 +32,12 @@ firebase.auth().onAuthStateChanged(function(user){
             
             firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
               .then(function() {
-                // Existing and future Auth states are now persisted in the current
-                // session only. Closing the window would clear any existing state even
-                // if a user forgets to sign out.
-                // ...
-                // New sign-in will be persisted with session persistence.
-                return firebase.auth().signInWithEmailAndPassword(email, password);
+                    return firebase.auth().signInWithEmailAndPassword(email, password);
               })
               .catch(function(error) {
                alert(error.message); 
               });
   
-            // firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-            //   // Handle Errors here.
-              
-            //     alert(error.message); 
-                
-            // });
         }
     }
 );
@@ -135,33 +46,18 @@ firebase.auth().onAuthStateChanged(function(user){
 
 $("#createBtn").click(function(){
 
-    var email = document.getElementById("createEmail").value; //var email = $("#createEmail").val();
+    var email = document.getElementById("createEmail").value; 
     
-    var password = document.getElementById("createPassword").value; //var password = $("#createPassword").val();
+    var password = document.getElementById("createPassword").value; 
     
     if(email !="" && password !=""){
         firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
           .then(function() {
-            // Existing and future Auth states are now persisted in the current
-            // session only. Closing the window would clear any existing state even
-            // if a user forgets to sign out.
-            // ...
-            // New sign-in will be persisted with session persistence.
-            return  firebase.auth().createUserWithEmailAndPassword(email, password);
+                return  firebase.auth().createUserWithEmailAndPassword(email, password);
           })
           .catch(function(error) {
            alert(error.message); 
           });
         
-        
-//          firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-//              // Handle Errors here.
-//              alert(error.message);
-             
-//             /*function changeCreateAccountState() {
-//                     document.getElementById("createMessage").innerHTML = "You have created an Account - Sign in!";
-//             }*/
- 
-// 		 });
 	}
 });
--- script.js
+++ script.js
@@ -12,12 +12,9 @@ firebase.initializeApp(config);
 firebase.auth().onAuthStateChanged(function(user){
         if(user){
             console.log('User ' + user.email+ 'logged in:');
-            // localStorage.setItem("currentUser", user);
-            // console.log("Hi"+localStorage.currentUser);
         }
         else{
             console.log('User not logged in :');
-            //send user to login page
             window.location.href ='index.html';
         }
         
@@ -43,153 +40,6 @@ function myFunction() {
     }
 }
     
-    
-    
-    
-    
-  /*  if (firebase.auth().currentUser) {
-        console.log('User Logged in :' + user );
-        window.location.href ='main.html';
-    } 
-    else{
-        console.log('User Not Logged in');
-        //window.location.href = 'index.html';
-    }*/
-
-
-/*firebase.auth().onAuthStateChanged(function(user){
-        if(user){
-            console.log('User Logged in :' + user );
-            window.location.href ='main.html';
-        }
-        else{
-            console.log('User Not Logged in');
-        }
-        
-    }
-);*/
-
-// /*Log in to Account*/
-// $("#login").click(
-//     function(){
-        
-//         //var username = $("#loginUsername").val();
-        
-//         var email = document.getElementById("loginEmail").value;//$("#loginEmail").val();
-        
-//         var password = document.getElementById("loginPassword").value; //$("loginPassword").val();
-        
-//         if(email !="" && password !=""){
-//             firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-//               // Handle Errors here.
-              
-//             var errorCode = error.code;
-//             var errorMessage = error.message;
-//              if (errorCode === 'auth/wrong-password') {
-              
-//                 $("#loginError").show().text(error.message);
-//              }
-//              else{
-//                  window.location.href = "/home.html";
-//                 $("#login").hide();
-//                 $("#logout").show();
-//              }
-                
-//             });
-//         }
-//     }
-// );
-
-// $("#createBtn").click(
-//     function(){
-
-//         var email = document.getElementById("createEmail").value; //var email = $("#createEmail").val();
-        
-//         var password = document.getElementById("createPassword").value; //var password = $("#createPassword").val();
-        
-//         if(email !="" && password !=""){
-            
-            
-//              firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-//                  // Handle Errors here.
-                 
-//                 function changeCreateAccountState() {
-//                         document.getElementById("createMessage").innerHTML = "You have created an Account - Sign in!";
-//                 }
-      
-//                 $("#createError").show().text(error.message);
-//                 //document.getElementById("createError").show().text(error.message);
-//               //$("#createError").show().text(error.message);
-//               // ...
-// 			 });
-// 		}
-// 	});
-    
-
-/*Logout of Account*/
-
-
-
-/*$("#logout").click(
-    function(){
-    firebase.auth().signOut().then(function() {
-      // Sign-out successful.
-    }).catch(function(error) {
-      // An error happened.
-    });
-    });*/
-
-/*firebase.auth().onAuthStateChanged(function(user){
-        if(user){
-           console.log('User Logged in :' + user );
-            window.location.href ='main.html';
-        }
-        else{
-            console.log('User Not Logged in');
-            //window.location.href = 'index.html';
-        }
-        
-    }
-);*/
-
-/*firebase.auth().onAuthStateChanged(user => {
-
-    if(user){
-        console.log('user is signed in', user);
-        //if(window.location.href != "/"){
-            //window.location='/';
-        //}
-    }
-    else{
-        console.log('user is NOT signed in');
-    }
-
-});*/
-
-/*logout.addEventListener('click', e => {
-    firebase.auth().signOut();
-});*/
-
-/*document.getElementById("logout").addEventListener("click", function(){
-    firebase.auth().signOut();
-});*/
-
-
-
-
-
-
-
-
-//document.getElementById("signOutBtn").addEventListener("click", mySignOut);
-
-/*function mySignOut(){
-    if (firebase.auth().currentUser) {
-        firebase.auth().signOut();
-    }
-}*/
-
-
 
 /* code for slider display value*/
 
@@ -239,115 +89,44 @@ $("#submitBtn").click(
     }
 );
 
-// firebase.auth().onAuthStateChanged(function(user){
-//         if(user){
-//             var data = localStorage.setItem('currentUser', user);
-//             console.log('User Logged in :' + user );
-//             //window.location.href ='main.html';
-//         }
-//         else{
-//             console.log('User Not Logged in');
-//         }
-        
-//     }
-// );
-
-// var user = firebase.auth().onAuthStateChanged;
-
-// if(user){
-//     console.log('User Logged in :' + user );
-//     sessionStorage.setItem("currentUser", user);
-//     console.log("Hi"+localStorage.currentUser);
-// }
-//  else{
-//         console.log('User Not Logged in' +user);
-//     }
-
-
-
-
-//  window.onload = function storage(){
-//      //console.log("Hi"+localStorage.currentUser);
-//     //var data = localStorage.getItem('currentUser');
-// }
-
-
-/*Make sure to set function to change then string in the form messages and change string of the "Login" in the navbar* - Lauren*/
 
 
 //Kellie's Contact Form
 
+var messagesRef = firebase.database().ref('Messages');
 
-// document.getElementById("contactForm").addEventListener("submit", submitForm);
+document.getElementById('contactForm').addEventListener('submit', submitForm);
 
+// Submit form
+function submitForm(e){
+  e.preventDefault();
 
-// function submitForm(){
-    
-//     // var name = getInputVal("name");
-//     // var email = getInputVal("email");
-//     // var message = getInputVal("message");
-    
-//     var name = document.getElementById("userName").value;
-//     var email = document.getElementById("userEmail").value;
-//     var message = document.getElementById("userMessage").value;
-    
-    
-//     // console.log(name, email, message);
-//     saveMessage(name, email, message);
+  // Get values
+  var name = document.getElementById('userName').value;
+  var email = document.getElementById('userEmail').value;
+  var message = document.getElementById('userMessage').value;
 
-//     //Refernece messages collection
-//     var messagesRef = firebase.database().ref("Messages");
-    
-    
-//     //Save message to firebase
-//     function saveMessage(name, email, message){
-//         var newMessageRef = messagesRef.push();
-//         newMessageRef.set({
-//             name: name,
-//             email: email,
-//             message:message
-//         });
-//     }
-// }
+  saveMessage(name, email, message);
+
+  // Alert
+  alert("Message sent!");
 
 
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
 
-
-// function submitForm(){
-    
-//     // //Get Values
-//     var name = getInputVal('name');
-//     var email = getInputVal('email');
-//     var message = getInputVal('message');
-    
-//     console.log(userName);
-    
-// }
-
-//saveMessage(name, email, message);
-
-// function getInputVal(id){
-//     return document.getElementById(id).value;
-// }
-
-// //Refernece messages collection
-// var messagesRef = firebase.database().ref("Messages");
-
-
-// //Save message to firebase
-// function saveMessage(name, email, message){
-//     var newMessageRef = messagesRef.push();
-//     newMessageRef.set({
-//         name: name,
-//         email: email,
-//         message:message
-//     });
-// }
-
-
-// function updateTextInput(val) {
-//           document.getElementById('textInput').value=val; 
-// }
+// Save message to firebase
+function saveMessage(name, email, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    // company:company,
+    email:email,
+    // phone:phone,
+    message:message
+  });
+}
 
 
 
